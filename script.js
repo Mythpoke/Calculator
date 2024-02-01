baa47ab2b64e8655552405aa7ef30652e9131a40
Maciek <maciek.jamroz@post.pl>
2024-02-01 19:06:40 +0100

    Add index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let numberFirst;
+let operator;
+let numberSecond;
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(numberFirst, operator, numberSecond) {
+   // if "+" call add() and etc.
+}
