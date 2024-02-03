fd0403ecfed11ae4700557b74766950c129ddb58
Maciek <maciek.jamroz@post.pl>
2024-02-03 19:36:19 +0100

    Css fixes

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
+const buttonsContainer = document.querySelector('.buttonsContainer');
 const buttons = document.querySelector('.buttons');
-const topRow = document.querySelector('.topRow');
-const middleRow = document.querySelector('.middleRow');
-const bottomRow = document.querySelector('.bottomRow');
 
-let arrayTop = ["CLEAR", "DEL", "/"];
-let arrayMiddle = ["7","8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "+"];
-let arrayBottom = ["C", "0", "="];
+let array = ["CLEAR", "DEL", "/","7","8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".","="]
 
-for(let i = 0; i < 3; i++) {
+for(let i = 0; i < 18; i++) {
     let button = document.createElement('button');
-    topRow.appendChild(button);
+    buttons.appendChild(button);
     button.classList.add('button');
-    button.textContent = arrayTop[i];
+    button.textContent = array[i];
     if (i === 0) button.setAttribute('style', 'width: 190px; background-color: red');
- }
-
- for(let i = 0; i < 12; i++) {
-    let button = document.createElement('button');
-    middleRow.appendChild(button);
-    button.classList.add('button');
-    button.textContent = arrayMiddle[i];
- }
-
- for(let i = 0; i < 3; i++) {
-    let button = document.createElement('button');
-    bottomRow.appendChild(button);
-    button.classList.add('button');
-    button.textContent = arrayBottom[i];
-    if (i === 2) button.setAttribute('style', 'width: 190px; background-color: green');
- }
-
-
+    if (i === 17) button.setAttribute('style', 'width: 190px; background-color: green');
+}
 
 let numberFirst;
 let operator;
